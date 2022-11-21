035679c042ea789b2974934b09f98dde8bd648f0
cynamon jules <julescweb@mail.com>
2022-11-21 11:30:59 +0100

    on rearange

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import '../styles/Home.module.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+export default function Home() {
+	return (
+		<>
+		
+
+		</>
+	)
+}
