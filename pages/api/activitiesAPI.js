f33123d335d6a0cc88056e72b31dabfd1391bd7f
cynamon jules <julescweb@mail.com>
2022-11-29 14:32:45 +0100

    petite correction

--- pages/api/activitiesAPI.js
+++ pages/api/activitiesAPI.js
@@ -6,8 +6,3 @@ import { API_URL } from '../../config/config';
 export function getActivities() {
     return axios.get(`${API_URL}/api/activities?populate=*`).then((res) => res.data);
 }
-
-// get one actuality by slug
-export function getActivity(id) {
-    return axios.get(`${API_URL}/api/activities/${id}?populate=*`).then((res) => res.data);
-}
