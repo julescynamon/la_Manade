9cc49a3c8e11ae06cac93a5e9032a1594dcce26b
jules cynamon <julescweb@gmail.com>
2023-01-22 10:52:28 +0100

    mise à jour des paquets, correction css,  mise en place du cookie consent

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import '../styles/global.css';
 import Layout from './components/layout';
+import CookieConsent from 'react-cookie-consent';
 
 function MyApp({ Component, pageProps }) {
     return (
         <Layout>
             <Component {...pageProps} />
+            <CookieConsent>
+                Ce site utilise des cookies pour améliorer l'expérience
+                utilisateur. En utilisant notre site, vous acceptez notre
+                politique de confidentialité.
+            </CookieConsent>
         </Layout>
     );
 }
